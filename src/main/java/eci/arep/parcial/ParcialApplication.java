08e5b6f22670eb7374ec2ab3fb8417495537beb8
MiltonGutierrez <gutierrezmilton31@gmail.com>
2025-03-28 11:06:52 -0500

    fix

--- src/main/java/eci/arep/parcial/ParcialApplication.java
+++ src/main/java/eci/arep/parcial/ParcialApplication.java
@@ -10,7 +10,7 @@ public class ParcialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ParcialApplication.class);
-		app.setDefaultProperties(Collections          .singletonMap("server.port", "8081"));
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
         app.run(args);
 	}
 
