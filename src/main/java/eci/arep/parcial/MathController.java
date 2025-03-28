e6234a725edeb29aa8b6267c59bcf91e4b6319f3
MiltonGutierrez <gutierrezmilton31@gmail.com>
2025-03-28 10:54:22 -0500

    MATHSERVICES

---
+++ src/main/java/eci/arep/parcial/MathController.java
@@ -0,0 +1,42 @@
+package eci.arep.parcial;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MathController {
+
+    private MathServices mathServices;
+
+    @Autowired
+    public MathController(MathServices mathServices){
+        this.mathServices = mathServices;
+    }
+    
+    @GetMapping("factors")
+    public ResponseEntity<Object> calculateFactors(@RequestParam(value="value") int value){
+        try {
+            return new ResponseEntity<>(Map.of("operation", "factors", "input", String.valueOf(value), "output", mathServices.factors(value)), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(Map.of("error", e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("primes")
+    public ResponseEntity<Object> calculatePrimes(@RequestParam(value="value") int value){
+        try {
+            return new ResponseEntity<>(Map.of("operation", "factors", "input", String.valueOf(value), "output", mathServices.primes(value)), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(Map.of("error", e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+        
+    }
+}
---
+++ src/main/java/eci/arep/parcial/ParcialApplication.java
@@ -0,0 +1,17 @@
+package eci.arep.parcial;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(ParcialApplication.class);
+		app.setDefaultProperties(Collections          .singletonMap("server.port", "8081"));
+        app.run(args);
+	}
+
+}
